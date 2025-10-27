c8300e462aac03dfee660c229e5dee9dbd28ec8f
Johan Yuri Martínez García <johanyuri24@gmail.com>
2025-10-27 17:36:49 -0600

    13-018 Material Autocomplete COMPLETADO

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
-  template: `
-    <p>
-      buscar works!
-    </p>
-  `,
+  templateUrl: './buscar.component.html',
   styles: [
   ]
 })
-export class BuscarComponent {
+
+export class BuscarComponent implements OnInit {
+  termino: string = '';
+  heroes: Heroe[] = [];
+
+  constructor(private heroesService: HeroesService) { }
+
+  ngOnInit(): void {
+  }
+
+  buscando() {
+    this.heroesService.getHeroes()
+    .subscribe( heroes => this.heroes = heroes );
+  }
 
 }
