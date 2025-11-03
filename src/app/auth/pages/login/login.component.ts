26c6c9ecb8b3abd5912a76ddb2ee2754f88db59d
ulisestc <ulisestc27@gmail.com>
2025-11-03 01:52:00 -0600

    15-004 Pantalla de Login Básico COMPLETADO

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  template: `
-    <p>
-      login works!
-    </p>
-  `,
+  templateUrl: './login.component.html',
   styles: [
   ]
 })
 export class LoginComponent {
 
+  constructor( private router: Router) { }
+
+  login() {
+    this.router.navigate(['./heroes']);
+  }
+
+  
 }
